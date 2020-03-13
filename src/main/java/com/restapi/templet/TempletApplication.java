45319b8ad4b62f474b572bd7ed53cd9c4c670849
황상일 <si8363@soongsil.ac.kr>
2020-03-13 13:56:49 +0900

    Init SpringBoot Project

---
+++ src/main/java/com/restapi/templet/TempletApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.templet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/templet/TempletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.templet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

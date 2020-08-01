260a7fbdd12c6b7b49c664183b2d2d96c8b16ae0
always0ne <si8363@naver.com>
2020-08-01 22:29:18 +0900

    refactor: Exception hander 추가, ErrorResponse 변경
    - 상태코드 규칙
    ```
    account  0000
    community 1000
    post  1100
    comment  1200
    system  2000
    
    ```

---
+++ src/main/java/com/restapi/template/community/post/PostExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.restapi.template.community.post;
+
+import com.restapi.template.community.post.exception.PostNotFoundException;
+import com.restapi.template.common.ErrorResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class PostExceptionHandler {
+
+    @ExceptionHandler(PostNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ErrorResponse handleCommentNotFound(PostNotFoundException exception) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND, 1101, exception.getMessage());
+    }
+}
